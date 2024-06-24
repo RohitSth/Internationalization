1a135de54a417c83a01035e7e1d3550517416700
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-06-24 15:15:27 +0545

    second commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <html>
+      <body className="text-center">
+        <h1 className="mt-10 font-semibold">Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+      </body>
+    </html>
+  );
+}
