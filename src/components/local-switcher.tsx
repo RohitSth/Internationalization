33828db26c343f6db30f234e52c11857e341ea7a
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-06-24 15:43:26 +0545

    minor update

--- src/components/local-switcher.tsx
+++ src/components/local-switcher.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useLocale } from "next-intl";
 import { useRouter } from "next/navigation";
 import { ChangeEvent, useTransition } from "react";
 
 const LocalSwitcher = () => {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
+
+  const localActive = useLocale();
 
   const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const nextLocale = e.target.value;
@@ -17,7 +20,12 @@ const LocalSwitcher = () => {
   return (
     <label className="border-2 rounded">
       <p className="sr-only">Change Language</p>
-      <select className="bg-slate-950 py-2" onChange={onSelectChange}>
+      <select
+        defaultValue={localActive}
+        className="bg-slate-950 py-2"
+        onChange={onSelectChange}
+        disabled={isPending}
+      >
         <option value="en">English</option>
         <option value="id">Indonesian</option>
       </select>
